3be886969ef17aad84cd6190eb9a588e8eb50bd7
Ngoc Huy <huybui479@gmail.com>
2020-09-01 20:37:07 +0700

    add docker & eviroment variables

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+require('dotenv').config();
 var app = express();
+//connect & test database
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb://${process.env.USER_ROOT_DATABASE}:${process.env.PASSWORD_ROOT_DATABASE}@localhost:${process.env.PORT_DATABASE}/train_c`, {useNewUrlParser: true, useUnifiedTopology: true,authSource:'admin'});
 
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'asdasd' });
+kitty.save().then(() => console.log('meow'));
+//
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
