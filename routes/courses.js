c6e9608d5001af2ff18a3907e1a201036874a26c
tantruong2303 <new9a11516@gmail.com>
2020-09-12 23:29:28 +0700

    add router update course

--- routes/courses.js
+++ routes/courses.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const isAdmin = require("../middleware/isAdmin");
 const create_course = require("../controllers/courses/create.course");
+const update_course = require("../controllers/courses/update.course");
 
 router.post("/", auth, isAdmin, create_course);
+router.put("/:name", auth, isAdmin, update_course);
 
 module.exports = router;
