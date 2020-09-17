a46fa302efda61d1f02eb9096901ee8c1ee9611f
tantruong2303 <new9a11516@gmail.com>
2020-09-17 17:38:23 +0700

    fix router create group

--- controllers/groups/create.group.js
+++ controllers/groups/create.group.js
@@ -7,18 +7,18 @@ const { hashingString } = require("../../helpers/bcrypt.helper");
 const validator = require("../../helpers/validator.helper");
 const validatorSchema = require("../../validators/group.validator");
 
-const createGroup = AsyncCatch(async (req, res, next) => {
-    const input = validator(validatorSchema(["name", "password", "course", "mentors"]), req.body);
+const createGroup = AsyncCatch(async (req, res) => {
+    const input = validator(validatorSchema(["name", "password", "course"]), req.body);
+    input.mentors = req.user.code;
+
     const group = await Group.findOne({ name: input.name });
     if (group) throw new BadRequest("Name is taken.");
 
     const course = await Course.findOne({ name: input.course });
     if (!course) throw new Unauthorized("Course is not correct.");
 
-    for (let mentor of input.mentors) {
-        const user = await User.findOne({ code: mentor });
-        if (!user) throw new Unauthorized("Mentors are not correct.");
-    }
+    const user = await User.findOne({ code: input.mentors });
+    if (!user) throw new Unauthorized("Mentors are not correct.");
 
     input.password = await hashingString(input.password);
     const result = await Group.create(input);
