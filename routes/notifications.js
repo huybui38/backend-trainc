7f90e29aeb95824a3d428316c05d7592d2496ed5
tantruong2303 <new9a11516@gmail.com>
2020-09-22 18:43:48 +0700

    add router delete notification

--- routes/notifications.js
+++ routes/notifications.js
@@ -4,8 +4,10 @@ const auth = require("../middleware/auth");
 const isAdmin = require("../middleware/isAdmin");
 const create_notification = require("../controllers/notifications/create.notification");
 const update_notification = require("../controllers/notifications/update.notification");
+const delete_notification = require("../controllers/notifications/delete.notification");
 
-router.post("/", auth, isAdmin, create_notification);
 router.put("/:id", auth, isAdmin, update_notification);
+router.delete("/:id", auth, isAdmin, delete_notification);
+router.post("/", auth, isAdmin, create_notification);
 
 module.exports = router;
