a0cfd9b7ccbc15fcdc90bc914aa60941f8d1bcfd
tantruong2303 <new9a11516@gmail.com>
2020-10-20 14:42:23 +0700

    a

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
-
+    //test
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     // Request headers you wish to allow
