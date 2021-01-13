ac24a5a93a5bc1700c6ec7b02981271a08fd02fb
tantruong2303 <63137673+tantruong2303@users.noreply.github.com>
2021-01-13 20:25:08 +0700

    Update getProfile.user.js

--- controllers/users/getProfile.user.js
+++ controllers/users/getProfile.user.js
@@ -1,14 +1,15 @@
 const { AsyncCatch } = require("../../helpers/utils.helper");
 const { NotFound } = require("../../helpers/errors.helper");
 const _ = require("lodash");
+const { User } = require("../../models/User.model");
 const validator = require("../../helpers/validator.helper");
 const validatorSchema = require("../../validators/user.validator");
 
 module.exports = AsyncCatch(async (req, res, next) => {
     const params = validator(validatorSchema(["code"]), req.params);
-    if (req.user.code !== params.code) throw new NotFound("Not found.");
+    const user = await User.findOne({ code: "params.code" });
+    if (!user) throw new NotFound("Not found.");
     
-    const user = _.pick(req.user, ["code", "name", "role", "point"]);
-    res.send(user);
+    const result = _.pick(user, ["code", "name", "role", "point"]);
+    res.send(result);
 });
-
