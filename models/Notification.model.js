b88674e358d5f7421b42882e7fe3cf40d03f8a77
tantruong2303 <new9a11516@gmail.com>
2021-01-26 15:06:26 +0700

    fix time

--- models/Notification.model.js
+++ models/Notification.model.js
@@ -10,7 +10,7 @@ const NotificationSchema = new mongoose.Schema({
     },
     createdTime: {
         type: Date,
-        default: formatDateOutput(Date.now()),
+        default: Date.now(),
     },
 });
 
